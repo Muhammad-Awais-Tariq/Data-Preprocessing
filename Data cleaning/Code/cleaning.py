4cbc840b984ac77412c02eee96ade17900e33859
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-18 18:19:34 +0500

    added the reuqired code

--- Data cleaning/Code/cleaning.py
+++ Data cleaning/Code/cleaning.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+from scipy import stats
+from mlxtend.preprocessing import minmax_scaling
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("F://Data-Preprocessing//Data//NFL Play by Play 2009-2017 (v4).csv")
 
@@ -16,3 +20,8 @@ dataset_without_null_coloumns = df.dropna(axis=1)
 dataset_with_filled_missing_values = df.fillna(0)
 
 df_nan_with_previous_value = df.bfill().fillna(0)
+
+original_goal_data = pd.DataFrame("kickstarters_2017.goal")
+
+scaled_goal_data = minmax_scaling(original_goal_data , columns = ["goal"])
+
